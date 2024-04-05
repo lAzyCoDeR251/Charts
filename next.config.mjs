cecc41b8a69c32e898f2815c6655cf75b35f1840
lAzyCoDeR251 <98582920+lAzyCoDeR251@users.noreply.github.com>
2024-04-05 11:45:45 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     async rewrites() {
-//         return [
-//           {
-//             source: '/api/:path*',
-//             destination: 'http://127.0.0.1:8000/:path*', // Proxy to Backend
-//           },
-//         ];
-//       },
-// };
-// export default nextConfig;
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   async rewrites() {
     return [
@@ -24,7 +10,25 @@ const nextConfig = {
       },
     ];
   },
+  output: "standalone"
 };
 
 export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   async rewrites() {
+//     return [
+//       {
+//         source: "/api/:path*",
+//         destination:
+//           process.env.NODE_ENV === "development"
+//             ? "http://127.0.0.1:8000/api/:path*"
+//             : "/api/",
+//       },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
 
